bf031be2dcba36b0158b86e80314fb3b94dd7603
retr0-r00t <enzoniches@gmail.com>
2026-08-09 21:39:38 -0300

    .

---
+++ src/main/java/com/task/niches/NichesApplication.java
@@ -0,0 +1,13 @@
+package com.task.niches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NichesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NichesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/niches/NichesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.niches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NichesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
