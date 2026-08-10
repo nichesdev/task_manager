be4268803b78e64cbfcbd6496aed0638c87a2d5c
retr0-r00t <enzoniches@gmail.com>
2026-08-10 03:51:06 -0300

    Task modules bulding update

--- src/main/java/com/taskmanager/niches/domain/task/model/TaskEntity.java
+++ src/main/java/com/taskmanager/niches/domain/task/model/TaskEntity.java
@@ -1,7 +1,11 @@
 package com.taskmanager.niches.domain.task.model;
 
+import com.taskmanager.niches.domain.users.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
@@ -13,15 +17,26 @@ import lombok.*;
 public class TaskEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false)
     private String title;
+
     @Column(columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
     private Priority priority;
+
     @Enumerated(EnumType.STRING)
     private StatusTaskEntity status;
+
+    private LocalDate dueDate;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
 }
--- src/main/java/com/taskmanager/niches/domain/task/repository/TaskRepository.java
+++ src/main/java/com/taskmanager/niches/domain/task/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.taskmanager.niches.domain.task.repository;
 
-public interface TaskRepository {
+import com.taskmanager.niches.domain.task.model.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
+    Optional<TaskEntity> findByTitleAndUserId(String title, Integer userId);
 }
