947e92963b5b77e43bd4a9cb26ef14ba2423714f
retr0-r00t <enzoniches@gmail.com>
2026-08-11 14:31:13 -0300

    category service

--- src/main/java/com/taskmanager/niches/domain/category/CategoryService.java
+++ src/main/java/com/taskmanager/niches/domain/category/CategoryService.java
@@ -1,4 +1,38 @@
 package com.taskmanager.niches.domain.category;
 
+import com.taskmanager.niches.domain.users.UserEntity;
+import com.taskmanager.niches.domain.users.UserRepository;
+import com.taskmanager.niches.exception.BadRequestException;
+import com.taskmanager.niches.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryResponseDto createCategory(CategoryRequestDto categoryRequestDto) throws NotFoundException, BadRequestException {
+        UserEntity user = userRepository.findById(categoryRequestDto.getUserId())
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
+
+        CategoryEntity existingCategory = categoryRepository
+                .findByNameAndUserId(categoryRequestDto.getCategoryName(), categoryRequestDto.getUserId())
+                .orElse(null);
+        if (existingCategory != null) {
+            throw new BadRequestException("Já existe uma categoria com este titulo para este Usuário.");
+        }
+        CategoryEntity category = CategoryEntity.builder()
+                .name(categoryRequestDto.getCategoryName())
+                .user(user)
+                .build();
+        CategoryEntity savedCategory = categoryRepository.save(category);
+
+        return CategoryResponseDto.builder()
+                .id(savedCategory.getId())
+                .name(savedCategory.getName())
+                .userId(savedCategory.getUser().getId())
+                .build();
+    }
 }
