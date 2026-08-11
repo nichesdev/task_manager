d6d45543af75ba9dbb60b46dd20050d43cb09b77
retr0-r00t <enzoniches@gmail.com>
2026-08-11 14:15:10 -0300

    category updates

--- src/main/java/com/taskmanager/niches/domain/category/CategoryRequestDto.java
+++ src/main/java/com/taskmanager/niches/domain/category/CategoryRequestDto.java
@@ -1,4 +1,20 @@
 package com.taskmanager.niches.domain.category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
 public class CategoryRequestDto {
+
+    @NotBlank
+    private String categoryName;
+    @NotNull
+    private Integer userId;
+
 }
--- src/main/java/com/taskmanager/niches/domain/category/CategoryResponseDto.java
+++ src/main/java/com/taskmanager/niches/domain/category/CategoryResponseDto.java
@@ -1,4 +1,16 @@
 package com.taskmanager.niches.domain.category;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
 public class CategoryResponseDto {
+
+    private Integer id;
+    private String name;
+    private Integer userId;
 }
