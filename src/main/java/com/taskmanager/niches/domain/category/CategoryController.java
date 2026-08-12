8085521f3bf4cc945180a66dcdf4bcfb0621489f
retr0-r00t <enzoniches@gmail.com>
2026-08-11 23:25:40 -0300

    controller

--- src/main/java/com/taskmanager/niches/domain/category/CategoryController.java
+++ src/main/java/com/taskmanager/niches/domain/category/CategoryController.java
@@ -1,4 +1,24 @@
 package com.taskmanager.niches.domain.category;
 
+import com.taskmanager.niches.exception.BadRequestException;
+import com.taskmanager.niches.exception.NotFoundException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/categories")
+@RequiredArgsConstructor
+@Validated
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponseDto createCategory(@Valid @RequestBody CategoryRequestDto categoryRequestDto) throws NotFoundException, BadRequestException {
+        return categoryService.createCategory(categoryRequestDto);
+    }
 }
